20128d2615341dea9d4ef5e37f911a2f433bd1e5
shadowsouls444 <angel.acuna.meza@gmail.com>
2026-04-22 15:32:00 -0500

    modelos en apps/usuarios terminados

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,11 +1,46 @@
 from django.db import models
 
-class Usuario(models.Model):
-    nombre = models.CharField()
-    apellido = models.CharField()
-    correo = models.EmailField()
-    password = models.CharField()
+class Rol(models.Model):
+    nombre_rol = models.CharField(max_length=80)
+    descripcion_rol = models.CharField(max_length=180)
 
-class Empresa(models.Model):
-    NIT = models.CharField(unique=True, null=False)
-    razon_social = models.CharField()
+    def __str__(self):
+        return f"Rol: {self.nombre_rol}"
+
+    class Meta:
+        db_table = "rol"
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    rol = models.ForeignKey(
+        Rol,
+        on_delete=models.PROTECT,
+        related_name='roles',
+        related_query_name='rol'
+    )
+
+    def __str__(self):
+        return f"Usuario: {self.nombre} {self.apellido}, {self.email}"
+
+    class Meta:
+        db_table = "usuario"   
+
+class Modulo(models.Model):
+    vista = models.CharField(max_length=80)
+    descripcion_modulo = models.CharField(max_length=180)
+
+    def __str__(self):
+        return f"Modulo: {self.vista}"
+    
+    ## De muchos a muchos
+    roles = models.ManyToManyField(
+        Rol,
+        related_name="modulos",
+        db_table="rol_modulo" ## Django por defecto crea una tabla intermedia con el nombre de las dos tablas relacionadas, pero podemos personalizarlo con db_table
+    )
+
+    class Meta:
+        db_table = "modulo"
--- apps/usuarios/serializers.py
+++ apps/usuarios/serializers.py
@@ -6,10 +6,6 @@ class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Usuario
         fields = ['nombre', 'apellido', 'correo', 'password'] ## LOS CAMPOS QUE QUEREMOS QUE RETORNE EN EL JSON
+        ## fields = '__all__' ## PARA INDICARLE QUE QUEREMOS QUE RETORNE TODOS LOS DATOS DEL MODELO EN EL JSON
         extra_kwargs = {'password': {'write_only': True}} ## ACA AGREGAMOS UNA REGLA DE CAMPO PARA QUE PASSWORD SOLO SE PUEDA ESCRIBIR PERO NO LEER EN EL JSON
-
-class EmpresaSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = models.Empresa
-        fields = '__all__' ## PARA INDICARLE QUE QUEREMOS QUE RETORNE TODOS LOS DATOS DEL MODELO EN EL JSON
+        
